492f49699bb2c34136efb2c0a3ab0e9d2aec2e9c
Absoul <Beatsmithtools65@yahoo.com>
2015-07-29 12:27:00 +0800

    Users added
    Added more design, a users functionality, etc

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resource :session
+  resources :users
+  root "movies#index"
+  resources :movies do
+    resources :reviews
+  end
+
+  get 'signup' => 'users#new'
+
+
+end
