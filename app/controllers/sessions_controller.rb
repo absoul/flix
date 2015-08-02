43e79e49ba92030dac438fa15d98a60cd8c162e5
Absoul <Beatsmithtools65@yahoo.com>
2015-08-02 18:43:50 +0800

    Login/out sessions added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,5 +14,10 @@ class SessionsController < ApplicationController
 			render :new
 		end
 	end
+
+	def destroy
+		session[:user_id] = nil
+		redirect_to root_url, notice: "You have successfully Logged Out!"
+	end
 	
 end
